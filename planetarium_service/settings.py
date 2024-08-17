049c55aba196941470695ed4f1608c7cb05dfa86
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-08-17 16:02:07 +0300

    move secret key to env

--- planetarium_service/settings.py
+++ planetarium_service/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--i8*cns*0=kbtf1&h51pqinw#9oe8ei+u6i4@*s793ofj)82zx"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
